b0640039cc892c41857c999a0c019635f2006c23
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-13 23:21:10 -0800

    Saved my progress.

--- logging/src/main.c
+++ logging/src/main.c
@@ -2,8 +2,19 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
+/* 
+    The output of the printk() is not deferred, meaning the output is 
+    sent immediately to the console without any mutual exclusion or 
+    buffering.
+    Logs are sent immediately as they are issued, and printk() 
+    will not return until all bytes of the message are sent
+*/
 int main(void)
 {
-    printk("Hello world!!!\n");
+    while(1)
+    {
+        printk("Hello world!!!\n");
+        k_msleep(1000);
+    }
     return 0;
 }
