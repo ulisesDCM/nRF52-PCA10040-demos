eabc7005da962d6324a42b107d08fe99c749d337
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-13 21:47:11 -0800

    Included 'myMath' library in the targets list to build.

--- minimal/src/main.c
+++ minimal/src/main.c
@@ -1,11 +1,19 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
+#include "myMath.h"
+
 void main(void)
 {
+    int a,b = 0;
+    
+    a = 10;
+    b = 20;
+
     while(1)
     {
         printk("Hello world!!!\n");
+        printk(" %d + %d = %d\n",a,b, sum(a,b));
         k_msleep(1000);
     }
 }
