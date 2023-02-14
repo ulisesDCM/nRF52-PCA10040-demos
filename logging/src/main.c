ca5ea362a07d2f2a4534ef9b2a5cc9aadef028b5
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-14 11:34:22 -0800

    Printed the factorial number using the printk function.

--- logging/src/main.c
+++ logging/src/main.c
@@ -2,6 +2,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
+#define MAX_FACTORIAL   10
 /* 
     The output of the printk() is not deferred, meaning the output is 
     sent immediately to the console without any mutual exclusion or 
@@ -9,11 +10,28 @@
     Logs are sent immediately as they are issued, and printk() 
     will not return until all bytes of the message are sent
 */
+void printFactorial(void)
+{
+    int factorial = 1;
+    for(uint8_t i=1; i<=MAX_FACTORIAL;  i++)
+    {
+        factorial =1;
+        for(uint8_t j=1; j<=i; j++)
+        {
+            factorial = factorial*j;
+        }
+        printk("The factorial of %2d = %d \n",i,factorial);
+    }
+}
+
 int main(void)
 {
+    printk("Hello World!!!\n");
+
     while(1)
     {
-        printk("Hello world!!!\n");
+        printk("Printing fatorial number to %d\n",MAX_FACTORIAL);
+        printFactorial();
         k_msleep(1000);
     }
     return 0;
