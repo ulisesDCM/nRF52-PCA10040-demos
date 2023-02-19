9a7a7fc67db3b2ee5f4bbf155d439a0d3ede100f
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-19 09:42:20 -0800

    Added project template for the uart-polling example.

---
+++ uart-polling/src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "zephyr/kernel.h"
+#include "zephyr/logging/log.h"
+
+// LOG_MODULE_DECLARE(main, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+int main(void)
+{
+    while(1)
+    {
+        LOG_INF("Hello world");
+        k_msleep(1000);
+    }
+    return 1;
+}
