fd86e5c649efce1c533cfc9e128efe85199ea2f9
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-19 13:55:39 -0800

    Added the configuration for the buttons.

--- atcmd-led/src/main.c
+++ atcmd-led/src/main.c
@@ -15,11 +15,18 @@ const struct gpio_dt_spec ledList[4] = {
     GPIO_DT_SPEC_GET(DT_ALIAS(ld4),gpios),
 };
 
+const struct gpio_dt_spec btnList[4] = {
+    GPIO_DT_SPEC_GET(DT_ALIAS(btn1), gpios),
+    GPIO_DT_SPEC_GET(DT_ALIAS(btn2), gpios),
+    GPIO_DT_SPEC_GET(DT_ALIAS(btn3), gpios),
+    GPIO_DT_SPEC_GET(DT_ALIAS(btn4), gpios),
+};
+
 static int led_init(void)
 {
     for(uint8_t i=0; i<4; i++)
     {
-        if(!device_is_ready(ledList[0].port))
+        if(!device_is_ready(ledList[i].port))
         {
             LOG_ERR("Error in the initialization of the LED %d",i);
             return 1;
@@ -30,13 +37,30 @@ static int led_init(void)
     return 0;
 }
 
+static int buttons_init(void)
+{
+    for(uint8_t i=0; i<4; i++)
+    {
+        if(!device_is_ready(btnList[i].port))
+        {
+            LOG_ERR("Error in the initialization of the Button %d",i);
+            return 1;
+        }
+
+        gpio_pin_configure_dt(&btnList[i], (GPIO_INPUT | GPIO_PULL_UP));
+    }
+
+    return 0;
+}
+
 int main(void)
 {
     if(led_init())
-    {
-        return 1;
-    }
+        return 1;   //Error
 
+    if(buttons_init())
+        return 1;   //Error
+        
     while(1)
     {
         LOG_INF("Hello world!!!");
