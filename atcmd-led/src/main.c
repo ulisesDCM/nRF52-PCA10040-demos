d53834997e0267f48bbdeb8271e177db2e8bc0e5
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-19 13:30:17 -0800

    Added project template for the LED control using AT command project.

---
+++ atcmd-led/src/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+int main(void)
+{
+    while(1)
+    {
+        LOG_INF("Hello world!!!");
+        k_msleep(1000);
+    }
+    
+    return 1;
+}
