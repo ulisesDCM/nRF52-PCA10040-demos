45bcf46827f4cad5d1453718b2225169ad334771
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-19 11:42:47 -0800

    Configured the UART0.

--- uart-interrupt/src/main.c
+++ uart-interrupt/src/main.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 
-#include <zephyr/zephyr.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+#include <zephyr/drivers/uart.h>
+
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(uart0));
+
+const struct uart_config uartConf = {
+    .baudrate=115200,
+    .data_bits=UART_CFG_DATA_BITS_8,
+    .flow_ctrl=UART_CFG_FLOW_CTRL_NONE,
+    .parity=UART_CFG_PARITY_NONE,
+    .stop_bits=UART_CFG_STOP_BITS_1
+};
+
 
 int main(void)
 {
+    if(!device_is_ready(uart))
+    {
+        LOG_ERR("Error in initialization of the UART0 port.");
+        return 1;
+    }
+
+    if(uart_configure(uart, &uartConf) < 0)
+    {
+        LOG_ERR("Error configuring the UART0 port.");
+        return 1;
+    }
+
+
     while(1)
     {
         LOG_INF("Hello world!!!");
