96f6aa5b62cba05067eb65297baf82cc8fd06870
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-19 14:14:19 -0800

    Configured the UART peripheral.

--- atcmd-led/src/main.c
+++ atcmd-led/src/main.c
@@ -22,6 +22,45 @@ const struct gpio_dt_spec btnList[4] = {
     GPIO_DT_SPEC_GET(DT_ALIAS(btn4), gpios),
 };
 
+const struct uart_config uartConfig = {
+    .baudrate=115200,
+    .data_bits=UART_CFG_DATA_BITS_8,
+    .flow_ctrl=UART_CFG_FLOW_CTRL_NONE,
+    .parity=UART_CFG_PARITY_NONE,
+    .stop_bits=UART_CFG_STOP_BITS_1
+};
+
+const struct device *uartDev = DEVICE_DT_GET(DT_NODELABEL(uart0));
+
+void serial_comm_cb(const struct device *dev, 
+                    struct uart_event *evt,
+                    void *user_data)
+{
+    switch (evt->type)
+    {
+    case UART_TX_DONE:
+        break;
+
+    case UART_TX_ABORTED:
+        break;
+
+    case UART_RX_RDY:
+        break;
+
+    case UART_RX_BUF_REQUEST:
+        break;
+
+    case UART_RX_BUF_RELEASED:
+        break;
+
+    case UART_RX_DISABLED:
+        break;
+
+    case UART_RX_STOPPED:
+        break;
+    }
+}
+
 static int led_init(void)
 {
     for(uint8_t i=0; i<4; i++)
@@ -53,6 +92,28 @@ static int buttons_init(void)
     return 0;
 }
 
+static int serial_comm_init(void)
+{
+    if(!device_is_ready(uartDev))
+    {
+        LOG_ERR("Error in the initialization of the UART0");
+        return 1;
+    }
+
+    if( uart_configure(uartDev, &uartConfig) < 0)
+    {
+        LOG_ERR("Error in the configuration of the UART0");
+        return 1;
+    }
+
+    if( uart_callback_set(uartDev, serial_comm_cb, NULL) < 0)
+    {
+        LOG_ERR("Error setting the UART callback function.");
+        return 1;
+    }
+    return 0;
+}
+
 int main(void)
 {
     if(led_init())
@@ -60,10 +121,12 @@ int main(void)
 
     if(buttons_init())
         return 1;   //Error
-        
+
+    if(serial_comm_init())
+        return 1;
+
     while(1)
     {
-        LOG_INF("Hello world!!!");
         k_msleep(1000);
     }
     
