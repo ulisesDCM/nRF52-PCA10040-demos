feda11a2f85a9a21befdfe73329293aed967fb1a
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-19 10:20:22 -0800

    Configured UART0 with 115200,8,N,1 and transmitting a character to the terminal.

--- uart-polling/src/main.c
+++ uart-polling/src/main.c
@@ -7,10 +7,36 @@
 // LOG_MODULE_DECLARE(main, LOG_LEVEL_DBG);
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
+const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(uart0));
+
+const struct uart_config uart0_config = {
+    .baudrate=115200,
+    .data_bits=UART_CFG_DATA_BITS_8,
+    .flow_ctrl=UART_CFG_FLOW_CTRL_NONE,
+    .parity=UART_CFG_PARITY_NONE,
+    .stop_bits=UART_CFG_STOP_BITS_1
+};
+
 int main(void)
 {
+    if(!device_is_ready(uart))
+    {
+        LOG_ERR("Error in the UART0 initialization");
+        return 1;
+    }
+
+    int err = uart_configure(uart, &uart0_config);
+    if (err == -ENOSYS)
+    {
+        LOG_ERR("Error configuring the UART0.");
+        return -ENOSYS;
+    }
+
+    unsigned char data = '1';
+
     while(1)
     {
+        uart_poll_out(uart, data);  //Transmitt data
         LOG_INF("Hello world");
         k_msleep(1000);
     }
