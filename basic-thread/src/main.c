9131a044eb2a4a72162bd3e351f76947518d2354
ulisesDCM <ulises.cmorales@hotmail.com>
2023-07-17 22:43:22 -0700

    Finish the first demo about basics threads

--- basic-thread/src/main.c
+++ basic-thread/src/main.c
@@ -5,8 +5,44 @@
  */
 
 #include <zephyr/kernel.h>
+#include "zephyr/logging/log.h"
 
-void main(void)
-{
-	printk("Hello World! %s\n", CONFIG_BOARD);
+/* Thread basic configuration */
+#define STACK_SIZE			(1024*1)
+#define THREAD0_PRIORITY	(6)
+#define THREAD1_PRIORIY		(6)
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+/* Thread functions */
+void thread0(void){
+	while(1){
+		LOG_INF("Hello, I am thread0 :)");
+		k_msleep(5);
+		// k_yield();
+	}
 }
+
+void thread1(void){
+	k_msleep(10);
+	while(1){
+		LOG_INF("Hello, I am thread1 ;)");
+		// k_yield();
+		k_msleep(5);
+	}
+}
+
+
+K_THREAD_DEFINE(thread0_id, STACK_SIZE, thread0, NULL, NULL, NULL, 
+					THREAD0_PRIORITY, 0, 0);
+	
+K_THREAD_DEFINE(thread1_id, STACK_SIZE, thread1, NULL, NULL, NULL, 
+					THREAD1_PRIORIY, 0, 0);
+
+/* Main thread loop */
+// void main(void)
+// {
+// 	LOG_INF("Hello World %s",CONFIG_BOARD);
+
+// 	/* Thread definitions */
+// }
