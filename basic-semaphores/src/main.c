23ca4d03000b2eb98000a2a925b2349acfa4f9c7
ulisesDCM <ulises.cmorales@hotmail.com>
2023-07-19 00:38:09 -0700

    Created all threads

--- basic-semaphores/src/main.c
+++ basic-semaphores/src/main.c
@@ -6,8 +6,52 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/random/rand32.h>
+#include <zephyr/kernel.h>
+
+
+#define PRODUCER_PRIORITY			(4)
+#define CONSUMER_PRIORITY			(5)
+#define PRODUCER_STACK_SIZE			(1024*2)
+#define CONSUMER_STACK_SIZE			(1024*2)
 
 LOG_MODULE_REGISTER(main,LOG_LEVEL_DBG);
+
+volatile uint32_t available_instance_count=10;
+
+void release_access(void){
+	available_instance_count++;
+	LOG_INF("Resource given and available_instance_count = %d",available_instance_count);
+
+}
+
+void get_access(void){
+	available_instance_count--;
+	LOG_INF("Resource taken and available_instance_count = %d",available_instance_count);
+}
+
+void producer(void){
+	LOG_INF("Producer thread started!!!");
+	while(1){
+		release_access();
+		k_msleep(500+sys_rand32_get()%10);
+	}
+}
+
+
+void consumer(void){
+	LOG_INF("Consumer thread started!!!");
+	while(1){
+		get_access();
+		k_msleep(sys_rand32_get()%10);
+	}
+}
+
+K_THREAD_DEFINE(consumer_thread, CONSUMER_STACK_SIZE, consumer, 
+						NULL,NULL,NULL,CONSUMER_PRIORITY, 0, 0);
+
+K_THREAD_DEFINE(producer_thread, PRODUCER_STACK_SIZE, producer, 
+						NULL,NULL,NULL,PRODUCER_PRIORITY, 0, 0);
 
 void main(void)
 {
