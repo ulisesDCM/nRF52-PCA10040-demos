c53f89f41697de2951639d1bc06689530495fd2b
ulisesDCM <ulises.cmorales@hotmail.com>
2023-07-27 23:47:22 -0700

    Initialized the status LED and toogling it every second.

--- ble-broadcast-non-connectable-advertising/src/main.c
+++ ble-broadcast-non-connectable-advertising/src/main.c
@@ -7,9 +7,38 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gap.h>
+
+#include <dk_buttons_and_leds.h>
+
+#define DEVICE_NAME					(CONFIG_BT_DEVICE_NAME)
+#define DEVICE_NAME_LEN				(sizeof(DEVICE_NAME)-1)
+
+#define RUN_STATUS_LED				(DK_LED1)
+#define RUN_LED_BLINK_TERMINAL		(1000)
+
+
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+// static const struct  bt_data_ad[]={
+// 	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+// };
 
 void main(void)
 {
-	LOG_INF("Hello World! %s", CONFIG_BOARD);
+	int blink_status=0;
+	int err;
+	
+	err=dk_leds_init();
+	if(err){
+		LOG_ERR("LEDs init failed (err %d)",err);
+		return;
+	}
+
+	LOG_INF("Advertising succesfully started");
+	while(1){
+		dk_set_led(RUN_STATUS_LED, (++blink_status)%2);
+		k_msleep(RUN_LED_BLINK_TERMINAL);
+	}
 }
