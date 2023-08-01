ba12df3dd189b294cd1fd4a863584a8cfd1d9afa
ulisesDCM <ulises.cmorales@hotmail.com>
2023-08-01 14:23:54 -0700

    Set the scan response structure and initialize the BLE and BLE advertising.

--- ble-broadcast-non-connectable-advertising/src/main.c
+++ ble-broadcast-non-connectable-advertising/src/main.c
@@ -20,14 +20,22 @@
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
-/* the advertising packet to send out.  */
+/* Defining the URL for send as a scan response */
+static unsigned char url_data[] = {0x17,'/','/','a','c','a','d','e','m','y','.',
+									'n','o','r','d','i','c','s','e','m','i','.',
+									'c','o','m'};
+
+/* the advertising packet to send. */
 static const struct  bt_data ad[]={
-	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+	/* Advertising flags */
+	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),				
+	/* Advertising packet data */
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),	
 };
 
+/* scan response  */
 static const struct bt_data sd[]={
-	
+	BT_DATA(BT_DATA_URI, url_data, sizeof(url_data))	
 };
 
 void main(void)
@@ -40,6 +48,25 @@ void main(void)
 		LOG_ERR("LEDs init failed (err %d)",err);
 		return;
 	}
+
+	/*Init BLE  */
+	err=bt_enable(NULL);
+	if(err){
+		LOG_ERR("BLE init failed (err %d)",err);
+		return;	
+	}
+	LOG_INF("Bluetooth initialized!!!");
+
+	/* Init BLE advertising mode */
+	err=bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	if(err){
+		LOG_ERR("BLE Advertising failed to start (err %d)\n",err);
+		return;
+	}
+
+	
+
+
 
 	LOG_INF("Advertising succesfully started");
 	while(1){
