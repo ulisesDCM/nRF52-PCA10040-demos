fce9394bee4edc37cf46a0510221de0d50516eb4
ulisesDCM <ulises.cmorales@hotmail.com>
2023-08-01 16:24:17 -0700

    Finish the demo project.

--- ble-dynamic-advertising/src/main.c
+++ ble-dynamic-advertising/src/main.c
@@ -20,6 +20,7 @@
 #define RUN_STATUS_LED				(DK_LED1)
 #define RUN_LED_BLINK_INTERVAL		(1000)
 
+#define COMPANY_ID_CODE				(0x0059)
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
@@ -27,10 +28,25 @@ static unsigned char url_data[] = {0x17,'/','/','a','c','a','d','e','m','y','.',
 									'n','o','r','d','i','c','s','e','m','i','.',
 									'c','o','m'};
 
+
+static struct bt_le_adv_param *adv_param = BT_LE_ADV_PARAM(BT_LE_ADV_OPT_NONE,
+												800,
+												801,
+												NULL
+											);
+
+typedef struct adv_mfg_data{
+	uint16_t company_code;		/* Company indentifier code */
+	uint16_t number_press;		/* Number of times button 1 is pressed */
+}adv_mfg_data_type;
+
+static adv_mfg_data_type adv_mfg_data = {COMPANY_ID_CODE, 0x00};
+
 /* Advertising package */
 static const struct bt_data ad[] ={
 	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN)
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+	BT_DATA(BT_DATA_MANUFACTURER_DATA, (unsigned char*) &adv_mfg_data, sizeof(adv_mfg_data))
 };
 
 /* Scan request package */
@@ -38,14 +54,40 @@ static const struct bt_data sd[] ={
 	BT_DATA(BT_DATA_URI, url_data, sizeof(url_data))
 };
 
+
+void button_changed(uint32_t button_state, uint32_t has_changed){
+	if(has_changed & button_state & USR_BUTTON){
+		adv_mfg_data.number_press +=1;
+		LOG_INF("Button 1 pressed, incrementing the counter to %d",adv_mfg_data.number_press);
+		bt_le_adv_update_data(ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	}
+}
+
+static int init_button(void){
+	int err=dk_buttons_init(button_changed);
+	if(err){
+		LOG_ERR("Button handler init error: %d",err);
+		return err;
+	}
+	return err;
+}
+
 void main(void)
 {
 	int err=dk_leds_init();
 	int led_state=0;
 
+	/* LED DK init */
 	if(err){
 		LOG_ERR("Led init error: %d",err);
 		return;
+	}
+
+	/* Button DK init */
+	err=init_button();
+	if(err){
+		LOG_ERR("Button init error: %d",err);
+		return ;
 	}
 
 	/* BLE init */
@@ -56,7 +98,7 @@ void main(void)
 	}
 
 	/* BLE advertising init */
-	err=bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	err=bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
 	if(err){
 		LOG_ERR("BT Adv error:%d",err);
 		return;
