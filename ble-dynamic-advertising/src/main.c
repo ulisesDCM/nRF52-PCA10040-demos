f369a8cd55976332b4eb4a1376666c6fdcc2986f
ulisesDCM <ulises.cmorales@hotmail.com>
2023-08-01 15:26:54 -0700

    Added basic configuration for the LOG and LEDs libraries

--- ble-dynamic-advertising/src/main.c
+++ ble-dynamic-advertising/src/main.c
@@ -5,8 +5,25 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#include "dk_buttons_and_leds.h"
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	int err=dk_leds_init();
+	int led_state=0;
+
+	if(err){
+		LOG_ERR("Led init error: %d",err);
+		return;
+	}
+
+	while(1){
+		LOG_INF("Hello World! %s\n", CONFIG_BOARD);
+		dk_set_led(DK_LED1, (++led_state)%2);
+		k_msleep(1000);
+	}
 }
